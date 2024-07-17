60f4419186c97911cabfbc2b897b33fdbe7d1dec
Ecipi <81295703+Ecipi@users.noreply.github.com>
2024-07-17 10:21:49 +0200

    Mise en place du CSS + changement des images et textes du slide.

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,29 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+
+
+const arrowLeft = document.getElementById('arrow_left');
+const arrowRight = document.getElementById('arrow_right');
+const slideImg = document.getElementById('slide');
+const slideTagLine = document.querySelector('#banner p');
+
+
+i = 0
+
+arrowLeft.addEventListener('click', () => {
+	console.log("Vous avez cliqué sur la flèche gauche");
+	i--
+	console.log(i);
+	slideImg.setAttribute('src', 'assets/images/slideshow/' + slides[i].image);
+	slideTagLine.innerHTML = slides[i].tagLine;
+});
+
+arrowRight.addEventListener('click', () => {
+	console.log("Vous avez cliqué sur la flèche droite");
+	i++
+	console.log(i);
+	slideImg.setAttribute('src', 'assets/images/slideshow/' + slides[i].image);
+	slideTagLine.innerHTML = slides[i].tagLine;
+});
