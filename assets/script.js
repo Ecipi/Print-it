d186c2ba222834a9f418e5503c365a30012cdec4
Ecipi <81295703+Ecipi@users.noreply.github.com>
2024-07-18 20:31:42 +0200

    Ajout de la fonction des dots.

--- assets/script.js
+++ assets/script.js
@@ -23,26 +23,30 @@ const arrowLeft = document.getElementById('arrow_left');
 const arrowRight = document.getElementById('arrow_right');
 const slideImg = document.getElementById('slide');
 const slideTagLine = document.querySelector('#banner p');
-
+let dots = document.getElementsByClassName('dot');
 
 i = 0
 
 function slideUp() {
+	dots[i].classList.remove('dot_selected')
 	i++
 	if (i >= slides.length) {
 		i = 0
 	}
 	slideImg.setAttribute('src', 'assets/images/slideshow/' + slides[i].image);
 	slideTagLine.innerHTML = slides[i].tagLine;
+	dots[i].classList.add('dot_selected')
 }
 
 function slideDown() {
+	dots[i].classList.remove('dot_selected')
 	i--
 	if (i < 0) {
 		i = slides.length - 1
 	}
 	slideImg.setAttribute('src', 'assets/images/slideshow/' + slides[i].image);
 	slideTagLine.innerHTML = slides[i].tagLine;
+	dots[i].classList.add('dot_selected')
 }
 
 arrowLeft.addEventListener('click', () => {
